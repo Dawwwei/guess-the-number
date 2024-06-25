d1ec888abe100bd4ddb34a8a38d8cd683c3c0845
Dawei Lin <personal.dawei.lin@gmail.com>
2024-06-26 00:14:07 +0200

    Create the website

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+let guess = 3;
+const generateRandomNumber = () => Math.floor(Math.random() * 20) + 1
+let randomNumber = generateRandomNumber()
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (!input.value) return alert('Please enter a number')
+    if (input.value != randomNumber) {
+        guess--
+        if (guess == 0) {
+            alert('You lost! The number was ' + randomNumber)
+            guess = 3
+            generateRandomNumber()
+        } else {
+            alert('Wrong! You have ' + guess + ' guesses left. Hint, the number is ' + (input.value > randomNumber ? 'smaller': "bigger"))
+        }
+    } else {
+        alert('You won! The number was ' + randomNumber)
+        guess = 3
+        generateRandomNumber()
+    }
+    input.value = ''
+})
